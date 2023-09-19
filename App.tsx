a956eacdd5b40cbc9a958ebde620cf4ec73d3522
jeet kanodia <jeetkanodia@gmail.com>
2023-09-20 04:13:15 +0530

    add elevated card and fancy card

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import {View, Text, SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
 import FlatCards from './components/FlatCards';
+import ElevatedCards from './components/ElevatedCards';
+import FancyCards from './components/FancyCards';
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
       <ScrollView>
         <View>
           <FlatCards />
+          <ElevatedCards />
+          <FancyCards />
         </View>
       </ScrollView>
     </SafeAreaView>
